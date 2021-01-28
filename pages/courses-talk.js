22e32579b6344dab4ca075d069aa141844912df0
Marcela Gotta <marcelagotta@gmail.com>
2021-01-28 19:04:40 +0100

    fix: fixing and changing fonts and fonmt size

--- pages/courses-talk.js
+++ pages/courses-talk.js
@@ -15,7 +15,7 @@ export default function CoursesTalk() {
         </h3>
         <p
           style={{
-            fontSize: "20px",
+            fontSize: "18px",
             margin: "0",
             marginBottom: "10px",
             color: "#e6362d",
