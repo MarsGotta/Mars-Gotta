ffe991d83b39ec028e222019c2d95e797c8422b3
marcela.gotta.contractor <marcela.gotta.contractor@bbva.com>
2021-11-12 18:57:33 +0100

    fix: footer text fixed

--- components/mars-footer/MarsFooter.jsx
+++ components/mars-footer/MarsFooter.jsx
@@ -14,7 +14,7 @@ const MarsFooter = ({
         target="_blank"
         rel="noopener noreferrer"
       >
-        Hecho con amor <span style={{margin: '0 15px 0 10px'}}>&#128156;</span> por Mars Gotta
+        Hecho con amor <span style={{margin: '0 10px'}}>&#128156;</span> por Mars Gotta
       </a>
     </footer>
   </>
