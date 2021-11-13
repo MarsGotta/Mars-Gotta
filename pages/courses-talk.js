44dfdf4fa427d27ee0903839f76b8c6ba4eb0070
marcela.gotta.contractor <marcela.gotta.contractor@bbva.com>
2021-11-13 22:46:20 +0100

    fix: new fixed added

--- pages/courses-talk.js
+++ pages/courses-talk.js
@@ -42,7 +42,7 @@ export default function CoursesTalk() {
         <div className={styles["talk-box"]}>
           <article className={styles["talk-element"]}>
             <h3 className={styles["talk-title"]}>
-              Charla sobre WebComponents
+              Una nueva web con WebComponents
             </h3>
             <span className={styles["talk-site"]}>
               Devcast Codemotion
