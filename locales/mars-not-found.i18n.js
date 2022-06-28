aef18a5903738cb27f29ba0fb5582bd973c23a73
marcela.gotta.contractor <marcela.gotta.contractor@bbva.com>
2022-06-28 12:51:06 +0200

    feat: added 404

---
+++ locales/mars-not-found.i18n.js
@@ -0,0 +1,12 @@
+export default {
+    es: {
+        headTitle: "404",
+        title: "¡Lo siento! Esta página no se ha encontrado.",
+        description: "Puedes volver al inicio haciendo click al logo ;)"
+    },
+    en: {
+        headTitle: "404",
+        title: "¡Lo siento! Esta página no se ha encontrado.",
+        description: "Puedes volver al inicio haciendo click al logo ;)"
+    },
+}
---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { useEffect } from "react";
+import { useConfigContext } from "../core/config-context";
+
+import locales from "../locales/mars-not-found.i18n.js";
+
+import MarsConstruction from "../components/mars-construction/MarsConstruction";
+
+export default function MarsNotFound(props) {
+  const { locale } = props;
+  const i18n = locales[locale];
+  const config = { title: i18n.headTitle };
+  const { configState, setConfigState } = useConfigContext();
+
+  useEffect(() => {
+    setConfigState({ ...configState, ...config });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{ config.title } | Mars Gotta</title>
+      </Head>
+      <MarsConstruction title={ i18n.title } description={ i18n.description } />
+    </>
+  );
+}
